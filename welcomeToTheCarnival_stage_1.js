99e5b0528e91b8443f5bea0d51d770316bcfc5cd
Chiwi <stanmma@gmail.com>
2022-10-09 14:31:08 +1000

    reverse stage 1

--- welcomeToTheCarnival_stage_1.js
+++ welcomeToTheCarnival_stage_1.js
@@ -1,17 +1,6 @@
 // Work on project. Stage 1/5: Welcome to the carnival!
 
-const giftList = {
-    1: {"Teddy Bear": 10},
-    2: {"Big Red Ball": 5},
-    3: {"Huge Bear": 50},
-    4: {"Candy": 8},
-    5: {"Stuffed Tiger": 15},
-    6: {"Stuffed Dragon": 30},
-    7: {"Skateboard": 100},
-    8: {"Toy Car": 25},
-    9: {"Basketball": 20},
-    10: {"Scary Mask": 10}
-};
+const giftList = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", "Stuffed Dragon", "Skateboard", "Toy Car", "Basketball", "Scary Mask"];
 
 function welcomeToCarnival () {
     console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!\nHello friend! Thank you for visiting the carnival!\nHere's the list of gifts:\n`);
