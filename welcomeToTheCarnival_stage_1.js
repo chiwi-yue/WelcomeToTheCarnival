4d619e9e4eb6c96e00f450c7e0e772c102d53c93
Chiwi <stanmma@gmail.com>
2022-10-09 14:19:32 +1000

    stage1

--- welcomeToTheCarnival_stage_1.js
+++ welcomeToTheCarnival_stage_1.js
@@ -1,1 +1,12 @@
 // Work on project. Stage 1/5: Welcome to the carnival!
+
+const giftList = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", "Stuffed Dragon", "Skateboard", "Toy Car", "Basketball", "Scary Mask"];
+
+function welcomeToCarnival () {
+    console.log(`WELCOME TO THE CARNIVAL GIFT SHOP!\nHello friend! Thank you for visiting the carnival!\nHere's the list of gifts:\n`);
+    for(let i in giftList) {
+        console.log(giftList[i]);
+    }
+}
+
+welcomeToCarnival ();
